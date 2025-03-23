ad6b6750cfbdffde9a62ff43e2677e88ddf11d2f
arefin008 <arefinrounok008@gmail.com>
2025-03-23 15:08:54 +0600

    first commit

---
+++ VisaRequestSystem/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using VisaRequestSystem.Models;
+
+namespace VisaRequestSystem.Controllers
+{
+    public class TokenController : Controller
+    {
+        // GET: Token
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GenerateToken(string visaType)
+        {
+            string token = TokenManager.GetToken(visaType);
+            ViewBag.Token = token;
+            ViewBag.Timestamp = DateTime.Now.ToString("g");
+            return View("Index");
+        }
+
+        public ActionResult Report()
+        {
+            var report = TokenManager.GetReport();
+            return View(report);
+        }
+    }
+}
---
+++ VisaRequestSystem/Models/TokenManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisaRequestSystem.Models
+{
+    public static class TokenManager
+    {
+        private static Dictionary<string, int> requestCount = new Dictionary<string, int>
+        {
+            { "MED", 0 },
+            { "TR", 0 },
+            { "BS", 0 },
+            { "GO", 0 }
+        };
+
+        private static int totalRequests = 0;
+        private static int currentTokenId = 0;
+
+        private const int MaxRequestsPerType = 25;
+        private const int MaxRequestsPerDay = 200;
+
+
+        private static List<string> reportLog = new List<string>();
+
+
+        public static string GetToken(string visaType)
+        {
+            string prefix;
+
+ 
+            switch (visaType)
+            {
+                case "Medical":
+                    prefix = "MED";
+                    break;
+                case "Tourist":
+                    prefix = "TR";
+                    break;
+                case "Business":
+                    prefix = "BS";
+                    break;
+                case "Govt":
+                    prefix = "GO";
+                    break;
+                default:
+                    throw new ArgumentException("Invalid visa type.");
+            }
+
+            if (totalRequests >= MaxRequestsPerDay)
+                return "Daily request limit reached.";
+
+
+            if (requestCount[prefix] >= MaxRequestsPerType)
+                return $"Request limit for {visaType} reached.";
+
+            requestCount[prefix]++;
+            totalRequests++;
+            currentTokenId++;
+
+
+            string token = $"{prefix}-{currentTokenId}";
+            reportLog.Add($"{DateTime.Now:G}: Token {token} generated for {visaType}");
+            return token;
+        }
+
+
+        public static void ResetCounts()
+        {
+            requestCount["MED"] = 0;
+            requestCount["TR"] = 0;
+            requestCount["BS"] = 0;
+            requestCount["GO"] = 0;
+            totalRequests = 0;
+        }
+
+        public static List<string> GetReport()
+        {
+            return new List<string>(reportLog);
+        }
+    }
+}
